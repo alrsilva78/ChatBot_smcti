3b3e5051f9733f8ff079c332f3ce40e33f2a9bda
Oraclle78 <informaticaalrsilva@hotmail.com>
2024-05-01 11:51:05 -0300

    adiciondo rosto ao bot

--- app.py
+++ app.py
@@ -3,27 +3,33 @@ import requests
 
 RASA_API_URL = "http://localhost:5005/webhooks/rest/webhook"
 
-app= Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template ('index.html')
+    return render_template("index.html")
 
-@app.route('/webhook', methods=['POST'])
+
+@app.route("/webhook", methods=["POST"])
 def webhook():
-    user_message = request.json['message']
+    user_message = request.json["message"]
     print("User Message:", user_message)
-    
+
     # Send user message to Tasa and get bot's response
-    rasa_response = requests.post(RASA_API_URL, json={'message': user_message})
+    rasa_response = requests.post(RASA_API_URL, json={"message": user_message})
     rasa_response_json = rasa_response.json()
-    
+
     print("Rasa Response:", rasa_response_json)
-    
-    bot_response = rasa_response_json[0]['text'] if rasa_response else 'Sorry I didn\'t understand that.'
-    
-    return jsonify({'response': bot_response})
-   
+
+    bot_response = (
+        rasa_response_json[0]["text"]
+        if rasa_response
+        else "Sorry I didn't understand that."
+    )
+
+    return jsonify({"response": bot_response})
+
 
 if __name__ == "__main__":
-    app.run(debug = True, port= 3000)
+    app.run(debug=True, port=3000)
